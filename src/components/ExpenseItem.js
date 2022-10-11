67fefbe9fad2134a3a3e2b87e3afb70114203183
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-11 17:56:46 +0300

    add styling.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,11 @@
-
-
-function ExpenseItem(){
-    return(
-        <h2>Expense Item!</h2>
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 28th 2021</div>
+            <div><h2>Car Insurance</h2></div>
+            <div>$256.32</div>
+        </div>
     )
 }
+
 export default ExpenseItem
