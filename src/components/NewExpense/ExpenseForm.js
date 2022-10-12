c04b95bc8505c9fdccaf711a4e90401108302093
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-12 07:01:57 +0300

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,32 +2,35 @@ import React , { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    // const [enteredTitle,setEnteredTitle] = useState("")
-    // const [enteredAmount,setEnteredAmount] = useState("")
-    // const [enteredDate,setEnteredDate] = useState("")
-    const [userInput, setUserInput] = useState({
-        enteredTitle:'',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+    const [enteredTitle,setEnteredTitle] = useState("")
+    const [enteredAmount,setEnteredAmount] = useState("")
+    const [enteredDate,setEnteredDate] = useState("")
+
+    // Another way
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+    // const titleChangeHandler = (event) => {
+    //     setUserInput((prevState) => {
+    //         return {
+    //             ...userInput,
+    //             enteredTitle: event.target.value
+    //         }
+    //     })
+    // }
 
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        })
+        setEnteredTitle(event.target.value)
     }
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        })
+        setEnteredAmount(event.target.value)
+
     }
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        })
+        setEnteredDate(event.target.value)
+
     }
 
     return(
