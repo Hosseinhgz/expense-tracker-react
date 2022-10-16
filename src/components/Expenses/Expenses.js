ed236dc7b1cbd7ebc8e1d55b7b35ef677ed3d629
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-17 02:31:25 +0300

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,28 +5,33 @@ import Card from '../UI/Card'
 import ExpensesFilter from '../Filter/ExpensesFilter'
 
 function Expenses(props){
-    const [yearFilter, setYearFilter] = useState(2021)
+    const [yearFilter, setYearFilter] = useState("2021")
     const yearFilterHandler= (selectedYear) => {
         setYearFilter(selectedYear)
         console.log(selectedYear)
     }
-    return(
-        <Card className='expenses'>
-            <ExpensesFilter
-                onSelectYearFilter={yearFilterHandler}
-                selected={yearFilter}
-            />
-            {props.expenses.map(item =>(
-                <ExpenseItem
-                    amount={item.amount}
-                    title={item.title}
-                    date={item.date}
-                    key={item.id}
-                />
-            ))}
-        </Card>
 
-    )
+    const filteredExpenses = props.expenses.filter( expense => {
+        return expense.date.getFullYear().toString()===yearFilter
+    })
+
+        return(
+            <Card className='expenses'>
+                <ExpensesFilter
+                    onSelectYearFilter={yearFilterHandler}
+                    selected={yearFilter}
+                />
+                {filteredExpenses.map(item =>(
+                    <ExpenseItem
+                        amount={item.amount}
+                        title={item.title}
+                        date={item.date}
+                        key={item.id}
+                    />
+                ))}
+            </Card>
+
+        )
 }
 
 export default Expenses
