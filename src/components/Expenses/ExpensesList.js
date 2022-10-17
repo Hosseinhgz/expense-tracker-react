d5a942fd0152aa9cc77a8100d67cf47e5b3e5d0f
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-17 04:16:30 +0300

    Update ExpensesList.js

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,21 +4,22 @@ import ExpenseItem from './ExpenseItem'
 
 const ExpensesList = (props) => {
     let expenseContent = <p>No expenses found.</p>;
-    if (props.items.length > 0) {
-        expenseContent = props.items.map(item =>(
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list__fallback">No expenses found.</h2>
+    }
+
+    return (
+        <ul className='expenses-list'>
+            {props.items.map(item =>(
             <ExpenseItem
                 amount={item.amount}
                 title={item.title}
                 date={item.date}
                 key={item.id}
             />
-        ))
-    }
+            ))}
+        </ul>
 
-    return (
-        <>
-            {expenseContent}
-        </>
     )
 }
 
